f565fd790660e013c9e1438b999580ed1dc95a9e
Albin Varghese <albin8285@gmail.com>
2022-07-17 13:17:25 +0530

    test: create basic tests for testing CyphFacade class

--- tests/CyphTest.php
+++ tests/CyphTest.php
@@ -2,7 +2,9 @@
 
 namespace Cyph\Cyph\Tests;
 
+use Cyph\Cyph\CyphFacade;
 use PHPUnit\Framework\TestCase;
+use RuntimeException;
 
 class CyphTest extends TestCase
 {
@@ -10,6 +12,13 @@ class CyphTest extends TestCase
     //test if phpunit is properly initialized
     public function test_init_phpunit()
     {
-        $this->assertTrue(1 == 2);
+        $this->assertTrue(1 == 1);
+    }
+
+    //initialize cyph class.
+    public function test_check_if_cyph_facade_class_is_instantiatable()
+    {
+        $this->expectException(RuntimeException::class);
+        new CyphFacade();
     }
 }
